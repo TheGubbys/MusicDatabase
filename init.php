e0c9ce8fb4ef9ca65d2ef4dd2daba11f25bea5f4
TheGubbys <elmevej18denni@live.dk>
2021-10-09 03:30:50 +0200

    Need help from teacher. Not sure how to set it up to work on own website.

--- init.php
+++ init.php
@@ -1,7 +1,7 @@
 <?php
 require "classes/classDB.php";
 
-define("CONFIG_LIVE", "0"); // 0: Test environment || 1: Live environment
+define("CONFIG_LIVE", "1"); // 0: Test environment || 1: Live environment
 
 if(CONFIG_LIVE == 0){
     $DB_SERVER = "localhost";
@@ -11,8 +11,8 @@ if(CONFIG_LIVE == 0){
 }else{
     $DB_SERVER = "mysql108.unoeuro.com";
     $DB_NAME = "gubby_dk_db";
-    $DB_USER = "";
-    $DB_PASS = "";
+    $DB_USER = "S398566";
+    $DB_PASS = "Gubby2863";
 }
 
 $db = new db($DB_SERVER, $DB_NAME, $DB_USER, $DB_PASS);
--- topheader.php
+++ topheader.php
@@ -1,5 +1,3 @@
-
-
 <nav class="navbar navbar-expand-md navbar-dark text-white bg-blue sticky-top">
     <div class="container">
         <a class="navbar-brand textstyle" href="index.php">
